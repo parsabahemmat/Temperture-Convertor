4277eb9e8f46c6ef331c34c8ec6e76ee3f452be2
Parsa_bh <bahemmat.parsa@gmail.com>
2022-12-02 14:50:49 +0330

    small changes

--- tempreturechange.m
+++ tempreturechange.m
@@ -7,21 +7,21 @@ while (~matches(unit , 'k')&& ~matches(unit , 'f')&& ~matches(unit , 'c'))
      unit = input('Enter the current unit:','s');
 if(matches(unit, 'c'))
      tempK = temp + 273.15;
-     tempC = 'units maches';
+     tempC = 'The units are same';
      tempF = (temp*1.8)+32;
 elseif (matches(unit, 'f'))
      tempC = (temp-32)*5.9;
      tempK = 5.9*(temp-32)+273.15;
-     tempF = 'units maches';
+     tempF = 'The units are same';
 elseif(matches(unit, 'k'))
      tempC = temp-273.15;
-     tempK = 'units maches';
+     tempK = 'The units are same';
      tempF = 1.8*(temp-273.15);
 else
     fprintf('The entered unit is not in the correct format!')
 end
 end
-tounit = input('enter the unit u want to be converted to:', 's');
+tounit = input('enter the unit u want it to be converted to:', 's');
 if(matches(tounit, 'c'))
     disp(tempC)
 elseif (matches(tounit, 'f'))
